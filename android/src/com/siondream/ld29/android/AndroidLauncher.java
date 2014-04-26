9d14d0dd514bd88fe52aa3624da703e5c9b7e5e4
David Saltares <david.saltares@gmail.com>
2014-04-26 08:00:58 +0100

    Adds libgdx project skeleton

---
+++ android/src/com/siondream/ld29/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.siondream.ld29.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.siondream.ld29.LudumDare;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LudumDare(), config);
+	}
+}
---
+++ html/src/com/siondream/ld29/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.siondream.ld29.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.siondream.ld29.LudumDare;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LudumDare();
+        }
+}
