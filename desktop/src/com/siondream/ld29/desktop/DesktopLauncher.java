247e70a5d1ac1072b53641be5e398d25953a1ccb
David Saltares <david.saltares@gmail.com>
2014-04-26 08:07:53 +0100

    Basic configuration

--- desktop/src/com/siondream/ld29/desktop/DesktopLauncher.java
+++ desktop/src/com/siondream/ld29/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.siondream.ld29.LudumDare;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Beneath the surface";
+		config.width = 1280;
+		config.height = 720;
+		
 		new LwjglApplication(new LudumDare(), config);
 	}
 }
